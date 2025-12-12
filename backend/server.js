63de5251966a1f2e3a280cbde83a68f0d5edb47d
Corviun Cybersecurity <noah@corviun.com>
2025-12-12 14:25:24 -0500

    add slash

--- backend/server.js
+++ backend/server.js
@@ -166,7 +166,7 @@ transporter.verify((error, success) => {
 // ---------------------
 // Serve frontend build (place **after API routes**)
 app.use(express.static(path.join(__dirname, "../dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
